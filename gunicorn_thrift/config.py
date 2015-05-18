1ab43c94ea567421da01d98359005b0fb57630c7
Tevin Zhang <mail2tevin@gmail.com>
2015-05-18 18:02:13 +0800

    clarify the name of service watcher

--- gunicorn_thrift/config.py
+++ gunicorn_thrift/config.py
@@ -93,8 +93,8 @@ class ClientConnected(Setting):
 
 
 class ServiceRegisterConf(Setting):
-    name = "service_register_conf"
-    section = "Service Register Conf"
+    name = "service_watcher_conf"
+    section = "Service Watcher Conf"
     default = {}
     validator = validate_dict
     desc = """\
@@ -102,10 +102,10 @@ class ServiceRegisterConf(Setting):
         """
 
 
-class ServiceRegisterClass(Setting):
-    name = "service_register_cls"
-    section = "Service Register Class"
-    cli = ["--service-register-cls"]
+class ServiceWatcherClass(Setting):
+    name = "service_watcher_cls"
+    section = "Service Watcher Class"
+    cli = ["--service-watcher-cls"]
     validator = validate_string
     default = ''
     desc = """\
